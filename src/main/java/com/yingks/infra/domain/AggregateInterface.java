e36fcea35e85d9a3758dda4da5097cd405ca03ee
nathena@qq.com <nathena@qq.com>
2016-07-27 10:32:41 +0800

    抽象接口

--- src/main/java/com/yingks/infra/domain/AggregateInterface.java
+++ src/main/java/com/yingks/infra/domain/AggregateInterface.java
@@ -2,12 +2,8 @@ package com.yingks.infra.domain;
 
 import java.util.List;
 
-import com.yingks.infra.domain.command.CommandInterface;
-
 public interface AggregateInterface {
 
-	public <T> void executeCommand(CommandInterface<T> command);
-	
 	public <T> void executeInsertCommand(T entity);
 	
 	public <T> void executeBatchInsertCommand(List<T> entitys);
@@ -28,6 +24,7 @@ public interface AggregateInterface {
 	public <T> T queryForEntity(Class<T> clazz,AggregateConditionInterface condition);
 	
 	public <T> List<T> queryForList(Class<T> clazz,AggregateConditionInterface condition);
+	
 	public <T> long queryForLong(Class<T> clazz,AggregateConditionInterface condition);
 	
 	public <T> Pagination<T> queryForPagination(Class<T> clazz,AggregateConditionInterface condition);
