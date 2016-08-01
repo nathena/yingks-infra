a7243b26766f22870866920c97fc1b62b72b74a8
nathena@qq.com <nathena@qq.com>
2016-08-01 14:15:47 +0800

    spring注入覆盖问题

--- src/main/java/com/yingks/infra/domain/BaseJdbcAggregateImpl.java
+++ src/main/java/com/yingks/infra/domain/BaseJdbcAggregateImpl.java
@@ -21,21 +21,13 @@ import com.yingks.infra.domain.query.JdbcSimpleQueryForPagination;
 
 public abstract class BaseJdbcAggregateImpl implements BaseAggregateInterface {
 
-	protected JdbcGeneralRepository generalRepository;
-	
-	public JdbcGeneralRepository getRepository() {
-		return generalRepository;
-	}
-
-	public void setRepository(JdbcGeneralRepository generalRepository) {
-		this.generalRepository = generalRepository;
-	}
+	public abstract JdbcGeneralRepository getRepository();
 	
 	@Override
 	public <T> void executeCommand(CommandInterface<T> command) {
 		
 		JdbcAbstractCommand<T> _command = (JdbcAbstractCommand<T>)command;
-		_command.setRepository(generalRepository);
+		_command.setRepository(this.getRepository());
 		_command.executeCommand();
 	}
 
@@ -43,35 +35,35 @@ public abstract class BaseJdbcAggregateImpl implements BaseAggregateInterface {
 	public <T> void executeInsertCommand(T entity) {
 		
 		JdbcInsertCommand<T> command = new JdbcInsertCommand<T>(entity);
-		command.setRepository(generalRepository);
+		command.setRepository(this.getRepository());
 		command.executeCommand();
 	}
 
 	@Override
 	public <T> void executeBatchInsertCommand(Class<T> clazz,List<T> entitys) {
 		JdbcBatchInsertCommand<T> command = new JdbcBatchInsertCommand<>(clazz,entitys);
-		command.setRepository(generalRepository);
+		command.setRepository(this.getRepository());
 		command.executeCommand();
 	}
 
 	@Override
 	public <T> void executeUpdateCommand(T entity) {
 		JdbcUpdateCommand<T> command = new JdbcUpdateCommand<T>(entity);
-		command.setRepository(generalRepository);
+		command.setRepository(this.getRepository());
 		command.executeCommand();
 	}
 
 	@Override
 	public <T> void executeUpdateByCommand(T entity, FilterInterface<T> filter) {
 		JdbcUpdateByCommand<T> command = new JdbcUpdateByCommand<T>(entity, filter);
-		command.setRepository(generalRepository);
+		command.setRepository(this.getRepository());
 		command.executeCommand();
 	}
 
 	@Override
 	public <T> void executeRemoveCommand(T entity) {
 		JdbcRemoveCommand<T> command = new JdbcRemoveCommand<>(entity);
-		command.setRepository(generalRepository);
+		command.setRepository(this.getRepository());
 		command.executeCommand();
 	}
 
@@ -79,21 +71,21 @@ public abstract class BaseJdbcAggregateImpl implements BaseAggregateInterface {
 	public <T> void executeRemoveByIdCommand(Class<T> clazz,Object idVal) {
 		
 		JdbcRemoveByIdCommand<T> command = new JdbcRemoveByIdCommand<>(clazz,idVal);
-		command.setRepository(generalRepository);
+		command.setRepository(this.getRepository());
 		command.executeCommand();
 	}
 
 	@Override
 	public <T> void executeRemoveByCommand(Class<T> clazz, FilterInterface<T> filter) {
 		JdbcRemoveByCommand<T> command = new JdbcRemoveByCommand<>(clazz,filter);
-		command.setRepository(generalRepository);
+		command.setRepository(this.getRepository());
 		command.executeCommand();
 	}
 
 	@Override
 	public <T> void executeStoreCommand(T entity) {
 		JdbcStoreCommand<T> command = new JdbcStoreCommand<T>(entity);
-		command.setRepository(generalRepository);
+		command.setRepository(this.getRepository());
 		command.executeCommand();
 	}
 	
@@ -101,7 +93,7 @@ public abstract class BaseJdbcAggregateImpl implements BaseAggregateInterface {
 	public <T> T queryForEntity(Class<T> clazz, Object idVal) {
 		
 		JdbcSimpleQueryForEntity<T> query = new JdbcSimpleQueryForEntity<T>(clazz,idVal);
-		query.setRepository(generalRepository);
+		query.setRepository(this.getRepository());
 		
 		return query.queryForEntity();
 	}
@@ -110,7 +102,7 @@ public abstract class BaseJdbcAggregateImpl implements BaseAggregateInterface {
 	public <T> T queryForEntity(Class<T> clazz, FilterInterface<T> filter) {
 		
 		JdbcSimpleQueryForEntity<T> query = new JdbcSimpleQueryForEntity<T>(clazz,filter);
-		query.setRepository(generalRepository);
+		query.setRepository(this.getRepository());
 		
 		return query.queryForEntity();
 	}
@@ -119,7 +111,7 @@ public abstract class BaseJdbcAggregateImpl implements BaseAggregateInterface {
 	public <T> List<T> queryForList(Class<T> clazz, FilterInterface<T> filter) {
 		
 		JdbcSimpleQueryForList<T> query = new JdbcSimpleQueryForList<T>(clazz,filter);
-		query.setRepository(generalRepository);
+		query.setRepository(this.getRepository());
 		
 		return query.queryForList();
 	}
@@ -128,7 +120,7 @@ public abstract class BaseJdbcAggregateImpl implements BaseAggregateInterface {
 	public <T> long queryForLong(Class<T> clazz, FilterInterface<T> filter) {
 		
 		JdbcSimpleQueryForLong<T> query = new JdbcSimpleQueryForLong<T>(clazz,filter);
-		query.setRepository(generalRepository);
+		query.setRepository(this.getRepository());
 		
 		return query.queryForLong();
 	}
@@ -137,7 +129,7 @@ public abstract class BaseJdbcAggregateImpl implements BaseAggregateInterface {
 	public <T> Pagination<T> queryForPagination(Class<T> clazz, FilterInterface<T> filter) {
 		
 		JdbcSimpleQueryForPagination<T> query = new JdbcSimpleQueryForPagination<T>(clazz,filter);
-		query.setRepository(generalRepository);
+		query.setRepository(this.getRepository());
 		
 		return query.queryForPagination();
 	}
--- src/main/java/com/yingks/infra/domain/query/JdbcAbstractQuery.java
+++ src/main/java/com/yingks/infra/domain/query/JdbcAbstractQuery.java
@@ -1,13 +1,11 @@
 package com.yingks.infra.domain.query;
 
-import com.yingks.infra.domain.data.EntityReflectUtils.ClassSpecification;
 import com.yingks.infra.domain.data.AbstractEntityAware;
 import com.yingks.infra.domain.data.JdbcGeneralRepository;
 
 public abstract class JdbcAbstractQuery<T> extends AbstractEntityAware<T>{
 
 	protected JdbcGeneralRepository repository;
-	protected ClassSpecification<T> entityClass;
 	
 	public JdbcAbstractQuery(Class<T> clazz) {
 		super(clazz);
