0e231d07a02fc11671c1a453a24612857b0dd412
nathena@qq.com <nathena@qq.com>
2016-08-03 14:37:45 +0800

    WoooX(https://github.com/WoooX) 修改了具体执行sql拼装时的代码使其支持排序和分页,去掉查询接口的泛型(没有必要?),增加查询接口的一个抽象子类。
    本地版本仓库移动，丢失github重新覆盖。

--- src/main/java/com/yingks/infra/context/Test.java
+++ src/main/java/com/yingks/infra/context/Test.java
@@ -1,36 +1,7 @@
 package com.yingks.infra.context;
 
-public class Test extends Test4 {
+public class Test {
 
-	public Test()
-	{
-	}
-	
-	public Test(int a)
-	{
-		
-	}
-	
-	public static void main(String[] arg)
-	{
-		Test test = new Test(1);
-	}
+
 }
 
-class Test4
-{
-	public Test4()
-	{
-		this.aaa();
-	}
-	
-	public Test4(int a)
-	{
-		System.out.println("121212");
-	}
-	
-	public void aaa()
-	{
-		System.out.println(this.getClass());
-	}
-}
