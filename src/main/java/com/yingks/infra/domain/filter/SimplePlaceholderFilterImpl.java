e3df1f1031d4cb6c9881837b667743e1ad23f3f0
nathena@qq.com <nathena@qq.com>
2016-08-08 16:36:47 +0800

    SimplePlaceholderFilterImpl，有bug还未修复

--- src/main/java/com/yingks/infra/domain/filter/SimplePlaceholderFilterImpl.java
+++ src/main/java/com/yingks/infra/domain/filter/SimplePlaceholderFilterImpl.java
@@ -18,4 +18,44 @@ public class SimplePlaceholderFilterImpl extends SimpleFilterImpl {
 		
 		_init(namedFitler,namedParams,null,null,null);
 	}
+	
+	//namedFitler xxx = :_0 and yyy = :_1 
+	public SimplePlaceholderFilterImpl(String namedFitler, String orderby, Object ...params)
+	{
+		Map<String,Object> namedParams = new HashMap<String, Object>();
+		int index = 0;
+		for(Object param : params )
+		{
+			namedParams.put("_"+index, param);
+			index++;
+		}
+		
+		_init(namedFitler,namedParams,orderby,null,null);
+	}
+	
+	public SimplePlaceholderFilterImpl(String namedFitler, String orderby, String group, Object ...params)
+	{
+		Map<String,Object> namedParams = new HashMap<String, Object>();
+		int index = 0;
+		for(Object param : params )
+		{
+			namedParams.put("_"+index, param);
+			index++;
+		}
+		
+		_init(namedFitler,namedParams,orderby,group,null);
+	}
+	
+	public SimplePlaceholderFilterImpl(String namedFitler, String orderby, String group, String having, Object ...params)
+	{
+		Map<String,Object> namedParams = new HashMap<String, Object>();
+		int index = 0;
+		for(Object param : params )
+		{
+			namedParams.put("_"+index, param);
+			index++;
+		}
+		
+		_init(namedFitler,namedParams,orderby,group,having);
+	}
 }
